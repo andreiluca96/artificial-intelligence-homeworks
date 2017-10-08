55ad4937d8a72a98b91019a6e048c4374426a344
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-10-08 16:35:36 +0300

    Added repeated question logic.

--- chat_bot.py
+++ chat_bot.py
@@ -6,9 +6,15 @@ kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
 
+annoyed_bot = aiml.Kernel()
+annoyed_bot.learn("std-startup_annoyed.xml")
+annoyed_bot.respond("load aiml b")
+
 conversation_metadata_filename = "conversation_metadata.yaml"
 
 session_id = 12345
+
+patterns_used = set()
 
 while True:
     message = raw_input("Enter your message to the bot: ")
@@ -19,25 +25,22 @@ while True:
     if message == "quit":
         exit()
     else:
-        bot_response = kernel.respond(message, session_id)
+        if message.lower() in patterns_used:
+            print (annoyed_bot.respond("annoyed", session_id))
+        else:
+            patterns_used.add(message.lower())
+            bot_response = kernel.respond(message, session_id)
 
-        print kernel.getPredicate('username', session_id)
-        print kernel.getPredicate('occupation', session_id)
-        print kernel.getPredicate('age', session_id)
+            if len(kernel.getPredicate('username', session_id)) > 0:
+                conversation_metadata[kernel.getPredicate('username', session_id)] = dict()
+                if len(kernel.getPredicate('occupation', session_id)) <= 0:
+                    conversation_metadata[kernel.getPredicate('username', session_id)]['occupation'] = ""
+                else:
+                    conversation_metadata[kernel.getPredicate('username', session_id)]['occupation'] = kernel.getPredicate('occupation', session_id)
 
-
-        if len(kernel.getPredicate('username', session_id)) > 0:
-            conversation_metadata[kernel.getPredicate('username', session_id)] = dict()
-            if len(kernel.getPredicate('occupation', session_id)) <= 0:
-                conversation_metadata[kernel.getPredicate('username', session_id)]['occupation'] = ""
-            else:
-                conversation_metadata[kernel.getPredicate('username', session_id)]['occupation'] = kernel.getPredicate('occupation', session_id)
-
-            if len(kernel.getPredicate('age', session_id)) <= 0:
-                conversation_metadata[kernel.getPredicate('username', session_id)]['age'] = ""
-            else:
-                conversation_metadata[kernel.getPredicate('username', session_id)]['age'] = kernel.getPredicate('age', session_id)
-        print (conversation_metadata)
-        write_dictionary(conversation_metadata, conversation_metadata_filename)
-
-        print (bot_response)
+                if len(kernel.getPredicate('age', session_id)) <= 0:
+                    conversation_metadata[kernel.getPredicate('username', session_id)]['age'] = ""
+                else:
+                    conversation_metadata[kernel.getPredicate('username', session_id)]['age'] = kernel.getPredicate('age', session_id)
+            write_dictionary(conversation_metadata, conversation_metadata_filename)
+            print (bot_response)
