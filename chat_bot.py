75267da87efff69895a2915f0f3a5cbb87d51f64
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-10-08 15:52:24 +0300

    Fixed the converstion metadata storage.

--- chat_bot.py
+++ chat_bot.py
@@ -10,23 +10,34 @@ conversation_metadata_filename = "conversation_metadata.yaml"
 
 session_id = 12345
 
-
-
 while True:
     message = raw_input("Enter your message to the bot: ")
 
     conversation_metadata = load_dictionary(conversation_metadata_filename)
-
+    if conversation_metadata is None:
+        conversation_metadata = dict()
     if message == "quit":
         exit()
     else:
         bot_response = kernel.respond(message, session_id)
 
-        if kernel.getBotPredicate('name') is not None:
-            conversation_metadata[kernel.getBotPredicate('name')]['occupation'] = kernel.getBotPredicate('occupation')
-            conversation_metadata[kernel.getBotPredicate('name')]['age'] = kernel.getBotPredicate('age')
+        print kernel.getPredicate('username', session_id)
+        print kernel.getPredicate('occupation', session_id)
+        print kernel.getPredicate('age', session_id)
 
+
+        if len(kernel.getPredicate('username', session_id)) > 0:
+            conversation_metadata[kernel.getPredicate('username', session_id)] = dict()
+            if len(kernel.getPredicate('occupation', session_id)) <= 0:
+                conversation_metadata[kernel.getPredicate('username', session_id)]['occupation'] = ""
+            else:
+                conversation_metadata[kernel.getPredicate('username', session_id)]['occupation'] = kernel.getPredicate('occupation', session_id)
+
+            if len(kernel.getPredicate('age', session_id)) <= 0:
+                conversation_metadata[kernel.getPredicate('username', session_id)]['age'] = ""
+            else:
+                conversation_metadata[kernel.getPredicate('username', session_id)]['age'] = kernel.getPredicate('age', session_id)
+        print (conversation_metadata)
         write_dictionary(conversation_metadata, conversation_metadata_filename)
-
 
         print (bot_response)
