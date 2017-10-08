a86f50a3153328ad57b9487344c647ca8812e39f
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2017-10-08 12:06:47 +0300

    Corrected script.

--- chat_bot.py
+++ chat_bot.py
@@ -2,6 +2,7 @@ import aiml
 
 kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
+kernel.respond("load aiml b")
 
 while True:
     message = raw_input("Enter your message to the bot: ")
@@ -9,3 +10,4 @@ while True:
         exit()
     else:
         bot_response = kernel.respond(message)
+        print (bot_response)
