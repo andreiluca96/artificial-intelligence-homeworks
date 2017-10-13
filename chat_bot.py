354a69f2c173a220cc69f3a9288344b8b058b925
DESKTOP-LEVSIBV\Ariana <opariuc.ariana@gmail.com>
2017-10-13 07:49:36 +0300

    created functions for metadata file initialization and bot replys

--- chat_bot.py
+++ chat_bot.py
@@ -26,6 +26,55 @@ def inactive_user():
     t = Timer(10, timeout)
     t.start()
 
+
+def conversation_medatadata_functionality():
+    if len(username) > 0:
+        if username in conversation_metadata \
+                and 'occupation' in conversation_metadata[username] \
+                and 'age' in conversation_metadata[username] \
+                and len(conversation_metadata[username]['occupation']) > 0 \
+                and len(conversation_metadata[username]['age']) > 0:
+            kernel.setPredicate('occupation', conversation_metadata[username]['occupation'], session_id)
+            kernel.setPredicate('age', conversation_metadata[username]['age'], session_id)
+        conversation_metadata[username] = dict()
+
+        if len(kernel.getPredicate('occupation', session_id)) <= 0:
+            conversation_metadata[username]['occupation'] = ""
+        else:
+            conversation_metadata[username]['occupation'] = kernel.getPredicate('occupation', session_id)
+
+        if len(kernel.getPredicate('age', session_id)) <= 0:
+            conversation_metadata[username]['age'] = ""
+        else:
+            conversation_metadata[username]['age'] = kernel.getPredicate('age', session_id)
+
+    write_dictionary(conversation_metadata, conversation_metadata_filename)
+
+
+def conversation_functionality():
+    if len(kernel.getPredicate('occupation', session_id)) and len(kernel.getPredicate('age', session_id)):
+        question = ' '
+
+        if username not in questions_asked:
+            questions_asked[username] = set()
+        questions_asked[username].add(' ')
+        while question in questions_asked[username] and len(questions_asked[username]) < 6:  # TO DO: change to 11
+            if randint(0, 1) == 0:
+                question = specialized_bot.respond(kernel.getPredicate('occupation', session_id))
+            else:
+                question = specialized_bot.respond(map_age(kernel.getPredicate('age', session_id)))
+        if question == ' ':
+            print "I don't know what should i tell you know."
+        else:
+            questions_asked[username].add(question)
+            questions_asked[username].remove(' ')
+            write_dictionary(questions_asked, questions_asked_filename)
+
+            print question
+    else:
+        print (bot_response)
+
+
 kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
@@ -49,10 +98,10 @@ questions_asked_filename = "questions_asked.yaml"
 session_id = 12345
 
 patterns_used = set()
-
+is_timeout = 0
+# inactive_user()
 while True:
     is_timeout = 1
-    # inactive_user()
     message = raw_input("user:")
 
     conversation_metadata = load_dictionary(conversation_metadata_filename)
@@ -67,56 +116,17 @@ while True:
         exit()
     else:
         if message.lower() in patterns_used:  # TO DO: Add persistence to these
+            is_timeout = 0
             print (annoyed_bot.respond("annoyed", session_id))
         else:
+            is_timeout = 0
             patterns_used.add(message.lower())
-
             message = get_similar_template(message.upper())
 
-            bot_response = kernel.respond(message, session_id)
+            if message is not None:
+                bot_response = kernel.respond(message, session_id)
 
             username = kernel.getPredicate('username', session_id)
-            if len(username) > 0:
-                if username in conversation_metadata \
-                        and 'occupation' in conversation_metadata[username] \
-                        and 'age' in conversation_metadata[username] \
-                        and len(conversation_metadata[username]['occupation']) > 0 \
-                        and len(conversation_metadata[username]['age']) > 0:
-                    kernel.setPredicate('occupation', conversation_metadata[username]['occupation'], session_id)
-                    kernel.setPredicate('age', conversation_metadata[username]['age'], session_id)
-                conversation_metadata[username] = dict()
+            conversation_medatadata_functionality()
+            conversation_functionality()
 
-                if len(kernel.getPredicate('occupation', session_id)) <= 0:
-                    conversation_metadata[username]['occupation'] = ""
-                else:
-                    conversation_metadata[username]['occupation'] = kernel.getPredicate('occupation', session_id)
-
-                if len(kernel.getPredicate('age', session_id)) <= 0:
-                    conversation_metadata[username]['age'] = ""
-                else:
-                    conversation_metadata[username]['age'] = kernel.getPredicate('age', session_id)
-
-            write_dictionary(conversation_metadata, conversation_metadata_filename)
-
-            if len(kernel.getPredicate('occupation', session_id)) and len(kernel.getPredicate('age', session_id)):
-                question = ' '
-
-                if username not in questions_asked:
-                    questions_asked[username] = set()
-                questions_asked[username].add(' ')
-                while question in questions_asked[username] and len(questions_asked[username]) < 6: # TO DO: change to 11
-                    if randint(0, 1) == 0:
-                        question = specialized_bot.respond(kernel.getPredicate('occupation', session_id))
-                    else:
-                        question = specialized_bot.respond(map_age(kernel.getPredicate('age', session_id)))
-
-                if question == ' ':
-                    print "I don't know what should i tell you know."
-                else:
-                    questions_asked[username].add(question)
-                    questions_asked[username].remove(' ')
-                    write_dictionary(questions_asked, questions_asked_filename)
-
-                    print question
-            else:
-                print (bot_response)
