b704bb004fec6518fd62c51992180e1b1ac0bb37
Luca <andrluc@c4b301d541b3.ant.amazon.com>
2017-11-04 18:53:12 +0200

    Added the state generator for 2 levels in the ComputerPawnChessBrain

--- Tema3 - Pawn Chess/src/com/company/PawnChessBrain.java
+++ Tema3 - Pawn Chess/src/com/company/PawnChessBrain.java
@@ -6,13 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PawnChessBrain {
-    private static final int NUMBER_OF_MOVES_AHEAD = 2;
     public static State chooseNextMove(State currentState) {
         StateTree stateTree = new StateTree(currentState);
 
-        for (int i = 0; i < NUMBER_OF_MOVES_AHEAD; i++) {
+        /**
+         * Generate the computer moves.
+         */
+        for (State state : currentState.generatePossibleState(false)) {
+            StateNode stateNode = new StateNode();
+            stateNode.state = state;
+            stateNode.parent = stateTree.root;
 
+            stateTree.root.children.add(stateNode);
         }
+
+        /**
+         * For every computer move, generate the human moves.
+         */
+        for (StateNode stateNode : stateTree.root.children) {
+            for (State state : stateNode.state.generatePossibleState(true)) {
+                StateNode newStateNode = new StateNode();
+                newStateNode.state = state;
+                newStateNode.parent = stateTree.root;
+
+                stateNode.children.add(stateNode);
+            }
+        }
+
+        List<Integer> humanMinumum = new ArrayList<>();
+
 
         return stateTree.root.state;
     }
@@ -25,11 +47,11 @@ class StateTree {
         root = new StateNode();
         root.state = rootData;
         root.children = new ArrayList<>();
+        root.parent = null;
     }
-
-    static class StateNode {
-        State state;
-        StateNode parent;
-        List<StateNode> children;
-    }
+}
+class StateNode {
+    State state;
+    StateNode parent;
+    List<StateNode> children = new ArrayList<>();
 }
